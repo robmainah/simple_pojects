b8975436f6f697179f8a46279c7c7eea6d7cdd70
robmainah <robmainah@gmail.com>
2023-05-04 16:54:50 +0300

    update data

--- user/models.py
+++ user/models.py
@@ -1,16 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Student(models.Model):
-    name = models.CharField(max_length=255)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     registration_no = models.CharField(max_length=255, unique=True)
     phone = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self) -> str:
-        return self.name
+        return f'{self.user.first_name} {self.user.last_name} profile'
--- user/views.py
+++ user/views.py
@@ -1,19 +1,42 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
-from .models import Student
-from .forms import ProfileRegistrationForm
+from .models import Profile
+from .forms import RegistrationForm, LoginForm
 
 def register(request):
     if request.method == 'POST':
-        form = ProfileRegistrationForm(request.POST)
+        form = RegistrationForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            data = {
+                'username': form.cleaned_data.get('email'),
+                'email': form.cleaned_data.get('email'),
+                'first_name': form.cleaned_data.get('first_name'),
+                'last_name': form.cleaned_data.get('last_name'),
+                'is_staff': False,
+            }
+
+            user = User.objects.create(**data)
+            form.save(user)
+
             messages.success(request, 'Registration successful')
             return redirect('user-register')    
     else:
-        form = ProfileRegistrationForm()
+        form = RegistrationForm()
 
     return render(request, 'user/register.html', { 'form': form })
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+
+            messages.success(request, 'Login successful')
+            return redirect('login')    
+    else:
+        form = LoginForm()
+
+    return render(request, 'user/login.html', { 'form': form })
