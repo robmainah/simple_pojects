45e1ca40dbb34b409e51d68e0dc1ac3918c9abae
robmainah <robmainah@gmail.com>
2023-05-04 21:23:02 +0300

    create login

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='user-register')
+    path('register/', views.register, name='register'),
+    path('login/', views.profile_login, name='login'),
+    # path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
 
 from .models import Profile
 from .forms import RegistrationForm, LoginForm
@@ -10,11 +12,13 @@ def register(request):
         form = RegistrationForm(request.POST)
 
         if form.is_valid():
+            # print(make_password(form.cleaned_data.get('password')))
             data = {
                 'username': form.cleaned_data.get('email'),
                 'email': form.cleaned_data.get('email'),
                 'first_name': form.cleaned_data.get('first_name'),
                 'last_name': form.cleaned_data.get('last_name'),
+                'password': make_password(form.cleaned_data.get('password')),
                 'is_staff': False,
             }
 
@@ -22,21 +26,32 @@ def register(request):
             form.save(user)
 
             messages.success(request, 'Registration successful')
-            return redirect('user-register')    
+            return redirect('login')    
     else:
         form = RegistrationForm()
 
     return render(request, 'user/register.html', { 'form': form })
 
-def login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-
-        if form.is_valid():
-
-            messages.success(request, 'Login successful')
-            return redirect('login')    
-    else:
+def profile_login(request):
+    if request.method == 'GET':
         form = LoginForm()
+        return render(request, 'user/login.html', { 'form': form })
 
-    return render(request, 'user/login.html', { 'form': form })
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        email = form.cleaned_data.get('email')
+        # password = make_password(form.cleaned_data.get('password'))
+        password = form.cleaned_data.get('password')
+        print(email)
+        print(password)
+        
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('register')
+        else:
+            messages.warning(request, 'Wrong credintials')
+            return redirect('login')
+ 
