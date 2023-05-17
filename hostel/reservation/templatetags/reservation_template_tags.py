fbbe50f97c3aa27dd35a691fef7869de8b9a5af7
robmainah <robmainah@gmail.com>
2023-05-17 20:39:52 +0300

    remove hostel from attendance

--- hostel/reservation/templatetags/reservation_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def starts_with(text, starts):
-    if text.startswith(starts):
-        return True
-    return False
